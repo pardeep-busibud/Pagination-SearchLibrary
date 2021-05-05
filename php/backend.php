dfa280c8df0866f8532124eabfc4b603e852b401
Nishu-WSB <nishu.garg@kdksoftware.com>
2021-05-05 11:42:19 +0530

    In the search field, we do not get any results if we enter an email address. We need to configure pagination library and search library in such a way that it helps us search using the email address as well.

--- php/backend.php
+++ php/backend.php
@@ -1,8 +1,8 @@
 <?php
 
-	require_once('/Applications/MAMP/htdocs/Mock_test_1/search_library/search.php');
+	require_once('../search_library/search.php');
 
-	require_once('/Applications/MAMP/htdocs/Mock_test_1/pagination1.0/prepared_query.php');
+	require_once('../pagination1.0/prepared_query.php');
 
 
 	$application_obj = new ManageApp();
@@ -13,7 +13,7 @@
 	$table_heading_name=array('Name','Email','Phone Number','Gender');
 	$table_column_name=array('name','email','phoneNum','gender');
 	$where=1;
-	if($_POST['request'] == 'data')
+	if($_POST['request'] === 'data')
 	{  
 		global $connection_mock_chat;
 	    $buffer_range=json_decode($_POST['buffer_data']);
@@ -24,6 +24,7 @@
 	    $where = 1;
 	    $response_data=array();
 	    $total_data=$application_obj->total_data($connection_mock_chat,$buffer_range,$data_per_page,$where);
+
 	    $response_data['total_length']=$total_data['total_length'];
 	    $response_data['total_data']=$total_data['total_data'];
 	    $response_data['max_page']=$total_data['max_page'];
@@ -31,12 +32,12 @@
 	    $response_data['table_column_name']=$table_column_name;
 	    echo json_encode($response_data);
 	}
-	elseif ($_POST['request']=='search') 
+	elseif ($_POST['request']==='search')
 	{   
 		global $connection_mock_chat;
 		global $application_obj;
 	    $params=array();
-	    $buffer_range=json_decode($_POST['buffer_data']);
+	    $buffer_range= json_decode($_POST['buffer_data'], true);
 	    $data_per_page=$_POST['data_per_page'];
 	    $input=$_POST['input_value'];
 	    $total_length=0;
@@ -44,39 +45,39 @@
 	    $response_data=array();
 	    $obj=new searching($input,$connection_mock_chat);
 	    $keys=array('type','table_name','search_col_name','get_colms','get_id');
-	    $value=array(array('string','login_db.mock_test_tbl','name','null as name,id,null as email,null as phone,null as gender','id'));
+	    $value=array(array('email','mock_test_tbl','email','Name as name,id,Email as email,Phone as phone,Gender as gender','id'));
 	    $query_data=array();
 
 	    foreach ($value as $key => $value1) 
 	    {
 	        $query_details=array_combine($keys, $value1);
-	        array_push($query_data, $query_details);
+	        $query_data[] = $query_details;
 	    }
 
 	    $get_query_and_data=$obj->get_query_and_data($query_data); 
 	    $result=array();
 	   
-	    if($get_query_and_data['query']!='')
+	    if($get_query_and_data['query']!=='')
 	    {  
 	        $result=mysqli_prepared_query($connection_mock_chat,$get_query_and_data['query'],"",$params);        
 	    }
 
 	    $get_ids=$obj->get_ids($result,$get_query_and_data['string'],$get_query_and_data['get_ids']);
-	   
+
 	    $where_data=$obj->searching_data($get_ids);
 
 	    $table_from=array("table_name_id","table_name_email");
-	    $table1_to=array("login_db.mock_test_tbl","login_db.mock_test_tbl");
+	    $table1_to=array("mock_test_tbl","mock_test_tbl");
 	    $tble1=str_replace($table_from, $table1_to, $where_data);
 
-	    if($tble1=='')
+	    if($tble1==='')
 	    {
 	        $total_data=array();
 	        echo json_encode($total_data);
 	    }
 	    else
 	    {
-	        $where=$tble1;
+	        $where= " mock_test_tbl.id IN (" . implode(',', array_column($get_ids, 'id')) . " )";
 
 	        $total_data=$application_obj->total_data($connection_mock_chat,$buffer_range,$data_per_page,$where);
 	        $response_data['total_length']=$total_data['total_length'];
@@ -90,14 +91,9 @@
 	}
 	Class ManageApp {
 
-		function MyConnection (&$connection,$host,$user,$db)
+		public function MyConnection (&$connection,$host,$user,$db)
 		{
-
-	        $host='localhost';
-
-	        $user='root';
-
-			$connection= mysqli_connect ($host, $user, "root" , $db); 
+		    $connection= mysqli_connect ($host, $user, "root" , $db);
 			if (!$connection) 
 			{
 				die ( "no connection found" . mysqli_error($connection));
@@ -105,13 +101,13 @@
 			
 		}
 		
-		function total_data($connection_mock_chat,$buffer_range,$data_per_page,$where)
+		public function total_data($connection_mock_chat,$buffer_range,$data_per_page,$where)
 		{
-		    $response_array=array();
+		    $response_array=[];
 		    $max_page=1;
 		    $test_length=0;
 		    $response_array_data=array();
-		    foreach ($buffer_range as $key => $value) 
+		    foreach ($buffer_range as $value)
 		    { 
 		        $range=explode(',', $value);
 		        $page_from=(int)$range[0];
@@ -119,7 +115,9 @@
 		        $data_from=$page_from*$data_per_page-$data_per_page;
 		        $data_to=$page_to-$page_from;
 		        $data_to=($data_to*$data_per_page)+$data_per_page;
+
 		        $data=$this->get_data($connection_mock_chat,$data_from,$data_to,$data_per_page,$where);
+
 		        if(!empty($data))
 		        {
 		            $max_page=$data[0]['max_page'];
@@ -153,7 +151,7 @@
 		                $per_page_array=array();
 		                for ($j=$from; $j<=$to; $j++) 
 		                { 
-		                    array_push($per_page_array,$data[$j]);
+		                    $per_page_array[] = $data[$j];
 		                    
 		                }
 		                $response_array[$i]=$per_page_array;
@@ -171,11 +169,11 @@
 		}  
 
 
-		function get_data($connection_mock_chat,$data_from,$data_to,$data_per_page,$where)
+		public function get_data($connection_mock_chat,$data_from,$data_to,$data_per_page,$where)
 		{   
 		    $response=array();
 		    $params=array();
-		    $max_page='';
+
 		    $query="SELECT COUNT(*)as total_row FROM mock_test_tbl WHERE ".$where;
 	        $total_row= mysqli_prepared_query($connection_mock_chat,$query,"",$params);
 	        $total_length=$total_row[0]['total_row'];
@@ -193,10 +191,10 @@
 		                $res_here=$val;
 		                $res_here['max_page']=$max_page;
 		                $res_here['total_length'] =$total_length;
-		                $Name=$val['name'];
-		                $Email=$val['email'];
-		                $phoneNum=$val['phone'];
-		                $Gender=$val['gender'];
+		                $Name=$val['Name'];
+		                $Email=$val['Email'];
+		                $phoneNum=$val['Phone'];
+		                $Gender=$val['Gender'];
 		                $res_here['name']=$Name;
 		                $res_here['email']=$Email;
 		                $res_here['phoneNum']=$phoneNum;
